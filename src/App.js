246d8fd1ed6ee55f04c3feec78db7e6c54563d73
ENZO GABRIEL BARBOSA FRANCO <enzofranco@dgtbr.com.br>
2023-04-04 17:51:18 -0400

    Adicionando cabecalho

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Header from './components/cabecalho';
+
+
+function App() {
+  return (
+    <Header />
+   
+  );
+}
+
+export default App;
--- src/components/cabecalho.js
+++ src/components/cabecalho.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import './styles.css';
-import logoEmpresa from '../imagens/logoEmpresa.png'
+// import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
 
 function Header() {
     return (
-        <header>
-                <img src={logoEmpresa} className='logoEmpresa'></img>
-            <h1 className='logo'> Angular Transportes Premium
-            </h1>
-            <nav>
-                <ul>
-                    <li>
-                        <a href='#'> Sobre Nós</a>
-                    </li>
-                    <li>
-                        <a href='#'> Contato</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <Box sx={{ flexGrow: 1 }}>
+            <AppBar position="fixed" style={{backgroundColor: "#ff751a"}}>
+                <Toolbar>
+                    <IconButton
+                        size="large"
+                        edge="start"
+                        color="inherit"
+                        aria-label="menu" 
+                        sx={{ mr: 4 }}>
+
+
+                    </IconButton>
+                    <Typography variant="h5" component="div" sx={{ flexGrow: 1 }} style={{color: "black"}}>
+                        Angular Transportes Premium
+                    </Typography>
+                    <Button color="inherit" sx={{ mr: 4 }}>Sobre Nós</Button>
+                    <Button color="inherit" sx={{ mr: 1 }}>Contatos</Button>
+                </Toolbar>
+            </AppBar>
+        </Box>
     );
 }
 
