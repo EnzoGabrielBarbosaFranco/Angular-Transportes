90db70176c0ff76aa95cef6a1da8b848062fc1d5
Enzo Gabriel <enzoddos7@gmail.com>
2023-04-24 14:11:54 -0400

    att

--- src/components/cabecalho.js
+++ src/components/cabecalho.js
@@ -23,7 +23,6 @@ function Header() {
                         Angular Transportes Premium
                     </Typography>
                     <Button color="inherit" sx={{ mr: 4 }}>Sobre Nós</Button>
-                    <Button color="inherit" sx={{ mr: 4 }}>Sobre Nós</Button>
                     <Button color="inherit" sx={{ mr: 1 }}>Contatos</Button>
                 </Toolbar>
             </AppBar>
