a5fb1e92f30f5256a8a60dedb9ab9b51461b4d97
Enzo Gabriel <enzoddos7@gmail.com>
2023-05-08 18:31:58 -0400

    Adição de modal telefone

--- src/components/cabecalho.js
+++ src/components/cabecalho.js
@@ -1,21 +1,50 @@
 // import Box from '@mui/material/Box';
 import AppBar from '@mui/material/AppBar';
+import * as React from 'react';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
-function Header() {
+
+export default function Header() {
+    const [open, setOpen] = React.useState(false);
+
+    const handleClickOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     return (
-        <AppBar component="nav" style={{backgroundColor: "#ff751a"}}>
+        <AppBar component="nav" style={{ backgroundColor: "#ff751a" }}>
             <Toolbar>
-  
-                <Typography variant="h4" component="div" fontFamily={'Train One'} sx={{ flexGrow: 1, ml: 4 }} padding={"35px"} style={{color: "black"}}>
+
+                <Typography variant="h4" component="div" fontFamily={'Train One'} sx={{ flexGrow: 1, ml: 4 }} padding={"35px"} style={{ color: "black" }}>
                     Angular Transportes Premium
                 </Typography>
                 <Button color="inherit" sx={{ mr: 4, fontSize: 15 }} fontFamily={'Train One'}>Sobre Nós</Button>
-                <Button color="inherit" sx={{ mr: 1, fontSize: 15 }} fontFamily={'Train One'}>Contatos</Button>
+                <Button color="inherit" sx={{ mr: 1, fontSize: 15 }} fontFamily={'Train One'} onClick={handleClickOpen}>Telefone</Button>
             </Toolbar>
+                <Dialog
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="alert-dialog-title"
+                    aria-describedby="alert-dialog-description">
+                    <DialogTitle id="alert-dialog-title">
+                        {"Telefone para contato:"}
+                    </DialogTitle>
+                    <DialogContent>
+                        <DialogContentText id="alert-dialog-description" sx={{  fontSize: 20 }} textAlign={'center'}>
+                        +55 (67) 9 9939-9900
+                        </DialogContentText>
+                    </DialogContent>
+                </Dialog>
         </AppBar>
     );
 }
-export default Header;
--- src/conteudo/BotaoContatos.js
+++ src/conteudo/BotaoContatos.js
@@ -11,7 +11,7 @@ import EmailIcon from '@mui/icons-material/Email';
 const actions = [
   { icon: <InstagramIcon />, name: 'Instagram', url: 'https://www.instagram.com/angulartransportespremium/' },
   { icon: <EmailIcon />, name: 'Email', url: 'angulartransportespremium@gmail.com' },
-  { icon: <LocalPhoneRoundedIcon />, name: 'Telefone',url:'' },
+  // { icon: <LocalPhoneRoundedIcon />, name: 'Telefone',url:'' },
   { icon: <DirectionsIcon />, name: 'Localização', url: 'https://www.google.com/maps/dir/-20.4306639,-54.5505696/Av.+Afonso+Pena,+4785+-+Santa+Fe,+Campo+Grande+-+MS,+79031-010/@-20.4494987,-54.6007196,13z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x9486e89588ee763d:0xa4d7688c3e686ef!2m2!1d-54.5911086!2d-20.4591406' },
 ];
 
