8b33063898b36801a33d21c93a28b12bb04e7a0c
Enzo Gabriel <enzoddos7@gmail.com>
2023-05-08 18:13:33 -0400

    Atualização sobre nós

--- src/conteudo/SobreNos.js
+++ src/conteudo/SobreNos.js
@@ -4,12 +4,15 @@ function SobreNos() {
     const sobrenos = {
         color: "white",
         backgroundColor: "#5570B4",
-        padding: "10px",
+        padding: "40px",
+        paddingTop: "80px",
+
     };
     const texto = {
         color: "white",
         backgroundColor: "#5570B4",
-        padding: "150px",
+        padding: "85px",
+        paddingTop: "40px",
     };
     const missao = {
         color: "white",
@@ -18,12 +21,13 @@ function SobreNos() {
     return (
         <Box component="main" fullWidth sx={{}}>
             <Toolbar />
-            <Typography variant="h4" textAlign={"center"} style={sobrenos} marginTop={20} sx={{ fontSize: 40 }} >
+            <Typography variant="h4" textAlign="center" style={sobrenos} marginTop={20} sx={{ fontSize: 40 }}>
                 Saiba mais sobre nós
             </Typography>
-            <Typography style={missao} textAlign={"center"} sx={{ fontSize: 18, lineHeight: 1.35 }}>Nossa Missão:
+            <Typography style={missao} textAlign="center" sx={{ fontSize: 18, lineHeight: 1.35 }}>
+                Nossa Missão:
             </Typography>
-            <Typography style={texto} textAlign={"center"} sx={{ fontSize: 15, lineHeight: 1.5 }}>
+            <Typography style={texto} textAlign="center" sx={{ fontSize: 15, lineHeight: 1.5, paddingTop: '20px' }}>
                 Aprendemos que um ótimo serviço começa e termina com profissionais experientes e amigáveis, o que explica
                 <br />
                 nosso rigoroso processo de contratação. Acreditamos que nossa equipe seja a melhor do setor e temos total
@@ -33,7 +37,7 @@ function SobreNos() {
                 <br />
                 ANGULAR TRANSPORTES PREMIUM LTDA busca finalizar cada reboque (guincho) com responsabilidade e o maior
                 <br />
-                nível de qualidade para passar credibilidade e confiança à seus clientes. Com foco em serviços personalizados,
+                nível de qualidade para passar credibilidade e confiança aos seus clientes. Com foco em serviços personalizados,
                 <br />
                 preços competitivos e satisfação do cliente, estamos sempre nos esforçando para atender e superar nossas
                 <br />
@@ -41,5 +45,6 @@ function SobreNos() {
             </Typography>
         </Box>
     );
+
 }
 export default SobreNos;
